fe64896f187d274176afe6db376b5aa570e55d45
Sandra <00001124067371sp@al.educacao.sp.gov.br>
2024-12-06 09:42:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da frança?',
+    'A capital da França é Paris.'
+)
+
+criaCartao(
+    'programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+)
